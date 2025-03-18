9721da3056c141dacefdffdee0e2014635bb9c4c
CatalyzoRR <mylmaz908@gmail.com>
2025-03-18 14:09:48 +0300

    Tests completed.

--- exercises/quizzes/quiz3.rs
+++ exercises/quizzes/quiz3.rs
@@ -12,18 +12,20 @@
 // block to support alphabetical report cards in addition to numerical ones.
 
 // TODO: Adjust the struct as described above.
-struct ReportCard {
-    grade: f32,
+struct ReportCard<T> {
+    grade: T,
     student_name: String,
     student_age: u8,
 }
 
 // TODO: Adjust the impl block as described above.
-impl ReportCard {
+impl<T: std::fmt::Display> ReportCard<T> {
     fn print(&self) -> String {
         format!(
             "{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade,
+            &self.student_name,
+            &self.student_age,
+            &self.grade
         )
     }
 }
@@ -43,10 +45,7 @@ mod tests {
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
         };
-        assert_eq!(
-            report_card.print(),
-            "Tom Wriggle (12) - achieved a grade of 2.1",
-        );
+        assert_eq!(report_card.print(), "Tom Wriggle (12) - achieved a grade of 2.1");
     }
 
     #[test]
@@ -56,9 +55,6 @@ mod tests {
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
-        assert_eq!(
-            report_card.print(),
-            "Gary Plotter (11) - achieved a grade of A+",
-        );
+        assert_eq!(report_card.print(), "Gary Plotter (11) - achieved a grade of A+");
     }
 }
